3c21eb99e59c647733b8846f7c9b4e22574ad05c
batsdats580-afk <batsdats580@gmail.com>
2026-06-08 16:30:26 -0400

    Add vignette.js script to the public directory

---
+++ public/vignette.js
@@ -0,0 +1,9 @@
+(function (s) {
+  s.dataset.zone = "11120208";
+  s.src = "https://n6wxm.com/vignette.min.js";
+})(
+  [document.documentElement, document.body]
+    .filter(Boolean)
+    .pop()
+    .appendChild(document.createElement("script"))
+);
