d73f0bdd12d54d666d35bf48773b8975bc8e1f4f
batsdats580-afk <batsdats580@gmail.com>
2026-06-08 17:17:45 -0400

    Delete public/vignette.js

--- public/vignette.js
+++
@@ -1,9 +0,0 @@
-(function (s) {
-  s.dataset.zone = "11120208";
-  s.src = "https://n6wxm.com/vignette.min.js";
-})(
-  [document.documentElement, document.body]
-    .filter(Boolean)
-    .pop()
-    .appendChild(document.createElement("script"))
-);
