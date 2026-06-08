abd9ff7b13b4bc331fa2f4862354c7579f47a050
batsdats580-afk <batsdats580@gmail.com>
2026-06-08 16:14:26 -0400

    Refactor script insertion for improved readability

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,21 +18,7 @@ export default function RootLayout({ children }) {
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
-               <script
-  dangerouslySetInnerHTML={{
-    __html: `
-      (function(s){
-        s.dataset.zone='11120208';
-        s.src='https://n6wxm.com/vignette.min.js';
-      })(
-        [document.documentElement, document.body]
-          .filter(Boolean)
-          .pop()
-          .appendChild(document.createElement('script'))
-      );
-    `,
-  }}
-/>
+        <script>(function(s){s.dataset.zone='11120208',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>  
         
         {/* ⭐ Adsterra Popunder Script */}
         <script src="https://pl29603713.effectivecpmnetwork.com/6d/d6/ba/6dd6ba5493682d87d257268189048b54.js"></script>
