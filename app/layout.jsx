642ac9f53fb76518a8b852f85a86ea5f8a0524a4
batsdats580-afk <batsdats580@gmail.com>
2026-06-08 16:04:55 -0400

    Rearrange Adsterra Popunder Script in layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,14 +18,7 @@ export default function RootLayout({ children }) {
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
-        
-        {/* ⭐ Adsterra Popunder Script */}
-        <script src="https://pl29603713.effectivecpmnetwork.com/6d/d6/ba/6dd6ba5493682d87d257268189048b54.js"></script>
-      </head>
-
-      <body className="min-h-screen bg-slate-50 text-slate-900 flex flex-col">
-        <Header />
-        <script
+               <script
   dangerouslySetInnerHTML={{
     __html: `
       (function(s){
@@ -40,6 +33,14 @@ export default function RootLayout({ children }) {
     `,
   }}
 />
+        
+        {/* ⭐ Adsterra Popunder Script */}
+        <script src="https://pl29603713.effectivecpmnetwork.com/6d/d6/ba/6dd6ba5493682d87d257268189048b54.js"></script>
+      </head>
+
+      <body className="min-h-screen bg-slate-50 text-slate-900 flex flex-col">
+        <Header />
+        
         {/* ⭐ Banner 728x90 (Visible Ad) */}
         <div className="flex justify-center my-4">
           <script
